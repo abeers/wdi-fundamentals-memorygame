4d8860998864fe60be79f6412624966a339bbf45
Alexander Beers <Alexander Beers>
2016-07-31 23:04:23 -0400

    added alert for results

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+
+if(cardOne === cardTwo) {
+	alert("You found a match!");
+}
+
+else {
+	alert("Sorry, try again");
+}
