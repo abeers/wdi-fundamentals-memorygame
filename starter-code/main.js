3859430f8a58465a3920e2593190860a71d27489
Alexander Beers <Alexander Beers>
2016-08-28 19:54:22 -0400

    Completes memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,44 @@
 console.log("JS file is connected to HTML! Woo!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-if(cardOne === cardTwo) {
-	alert("You found a match!");
+var gameBoard = document.getElementById("game-board");
+createBoard();
+
+function createBoard() {
+	for(var i = 0; i < cards.length; i++) {
+		var card = document.createElement('div');
+		card.className = "card";
+		card.setAttribute('data-card', cards[i]);
+		gameBoard.appendChild(card);
+		card.addEventListener('click', isTwoCards);
+	}
 }
 
-else {
-	alert("Sorry, try again");
+//checks to see if there are cards in play
+function isTwoCards() {
+
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+  }
+
+}
+
+function isMatch(cardsInPlay) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else alert("Sorry, try again");
 }
